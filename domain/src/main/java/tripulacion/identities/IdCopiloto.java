34fbaca059a3e93fa31d25474f0453c3c82dce11
luis <luisfelorivas@gmai.com>
2022-05-20 19:43:30 -0500

    Actualizacion IdCopiloto: se agrego contructor y metodo "of"

--- domain/src/main/java/tripulacion/identities/IdCopiloto.java
+++ domain/src/main/java/tripulacion/identities/IdCopiloto.java
@@ -3,4 +3,12 @@ package tripulacion.identities;
 import co.com.sofka.domain.generic.Identity;
 
 public class IdCopiloto extends Identity {
+
+    public IdCopiloto(String uuid) {
+        super(uuid);
+    }
+
+    public static IdCopiloto of(String id) {
+        return new IdCopiloto(id);
+    }
 }
